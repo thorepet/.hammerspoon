8d38c9ae5d32ef9c327037ec8f237e2764b2dcc4
thorepet <petersen.thore@gmail.com>
2020-09-01 18:44:26 +0200

    Make DND toggle script prettier.

--- mac-dnd.sh
+++ mac-dnd.sh
@@ -1,11 +1,32 @@
 #!/bin/bash
 
 # Enable or disable the Do Not Disturb mode in macOS. The script edits the
-# notification center's .plist file to toggle the mode. A bug is that when
-# disabling the mode, the notification center's icon in the menu bar stays
-# greyed out. 
+# notification center's .plist file to toggle the mode. 
+# A bug is that when disabling the mode, the notification center's icon in the
+# menu bar stays greyed out. 
 
-# based on: https://gist.github.com/ryangreenberg/5267f68a8e7b07ea66370b4eb5580ab9
+NCPATH=~/Library/Preferences/ByHost/com.apple.notificationcenterui
+DATE="`date -u +\"%Y-%m-%d %H:%M:%S +000\"`"
+
+if [ $1 -eq 1 ]
+then
+  defaults -currentHost write $NCPATH doNotDisturb -boolean true
+  defaults -currentHost write $NCPATH doNotDisturbDate -date "$DATE"
+
+  killall NotificationCenter
+fi
+
+if [ $1 -eq 0 ]
+then
+  defaults -currentHost write $NCPATH doNotDisturb -boolean false
+  
+  # When deactivating "by hand" the doNotDisturbDate field is removed.
+  defaults -currentHost delete $NCPATH doNotDisturbDate
+
+  killall NotificationCenter
+fi
+
+# https://gist.github.com/ryangreenberg/5267f68a8e7b07ea66370b4eb5580ab9
 # 
 # original code:
 # #!/bin/bash
@@ -25,22 +46,3 @@
 #   killall NotificationCenter
 #   echo "Do Not Disturb is disabled. Run $0 to turn it on again."
 # fi
-
-NCPATH = ""
-
-if [ $1 -eq 1 ]
-then
-  defaults -currentHost write ~/Library/Preferences/ByHost/com.apple.notificationcenterui doNotDisturb -boolean true
-  defaults -currentHost write ~/Library/Preferences/ByHost/com.apple.notificationcenterui doNotDisturbDate -date "`date -u +\"%Y-%m-%d %H:%M:%S +000\"`"
-  killall NotificationCenter
-fi
-
-if [ $1 -eq 0 ]
-then
-  defaults -currentHost write ~/Library/Preferences/ByHost/com.apple.notificationcenterui doNotDisturb -boolean false
-  
-  # When deactivating "by hand" the doNotDisturbDate field is removed, this was
-  # missing in the original code.
-  defaults -currentHost delete ~/Library/Preferences/ByHost/com.apple.notificationcenterui doNotDisturbDate
-  killall NotificationCenter
-fi
