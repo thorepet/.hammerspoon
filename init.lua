a67656b8ceabb9ee130bdd7de5daa6493f6c2544
thorepet <petersen.thore@gmail.com>
2020-09-18 09:24:29 +0200

    App launch watcher comments.

--- init.lua
+++ init.lua
@@ -12,3 +12,26 @@ end
 require("muteIfEduroam")
 require("modes")
 require("screenLock")
+
+
+-- https://github.com/scottcs/dot_hammerspoon/tree/2495d931782fb485459a254bb62557b93329ac14
+
+-- function appWatchCallback(appName, eventType, appObject)
+
+--     local function hasNoMainWindow() return appObject:mainWindow() == nil end
+
+--     if appName == "Visual Studio Code" then
+--         if eventType == hs.application.watcher.launched then
+--             hs.timer.waitWhile(
+--                 hasNoMainWindow, 
+--                 function()
+--                     appObject:mainWindow():setFullScreen(true)
+--                 end
+--             )
+--         end
+--     end
+
+-- end
+
+-- vsCodeWatcher = hs.application.watcher.new(appWatchCallback)
+-- vsCodeWatcher:start()
