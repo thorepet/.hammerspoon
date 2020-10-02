e91f669eee918e85ea297062ca998743dd39686a
thorepet <petersen.thore@gmail.com>
2020-10-02 09:40:05 +0200

    Simplify screen selection.

--- officeFullscreen.lua
+++ officeFullscreen.lua
@@ -3,36 +3,27 @@
 
 -- Function to move the focussed window to my second screen and toggle full
 -- screen mode.
-function window_fullscreen2()
-    -- Potentially overcomplicated, but works for now.
-    
-    -- Check if the second screen is "my" office screen. If so, return the
-    -- hs.screen object. If not return nil.
+function win_office_fullscreen()
+
+    -- Find my office screen. Returns hs.screen object, or nil.
     function office_screen()
-
-        -- Return bool of the function's argument being "my" office screen.
-        function is_office_screen(screen)
-            UUID = "1AE2449E-4F45-5E50-AEA5-2629374967EE"
-            return screen:getUUID() == UUID
-        end
-    
-        screen = hs.screen.allScreens()[2]
-        if is_office_screen(screen) then
-            return screen
-        else
-            return nil
-        end
+        UUID = "1AE2449E-4F45-5E50-AEA5-2629374967EE"
+        return hs.screen.find(UUID)
     end
-
-    win = hs.window.focusedWindow()
+    
     screen = office_screen()
-
+    
     -- If screen is not nil, move the currently focussed window to it, and make
     -- it full screen.
     if screen then
+        win = hs.window.focusedWindow()
+
+        -- Exit fullscreen, otherwise :moveToScreen() does not work.
+        win:setFullScreen(false)
+
         win:moveToScreen(screen)
         win:setFullScreen(true)
     end
 end
 
-hs.hotkey.bind(hyper, "F", window_fullscreen2)
+hs.hotkey.bind(hyper, "F", win_office_fullscreen)
